0952579f8bc6b5bc4c0c862d3c2888f2b49c2b5b
elainapolson <elainapolson@gmail.com>
2015-06-17 09:15:58 -0400

    done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+
+class Anagram
+
+  def initialize(string)
+    @string = string
+    @matches = []
+  end
+
+  def match(array)
+    array.each do |word|
+      if word.chars.sort.join == @string.chars.sort.join
+        @matches << word
+      end
+    end
+    @matches.compact
+  end
+
+end
